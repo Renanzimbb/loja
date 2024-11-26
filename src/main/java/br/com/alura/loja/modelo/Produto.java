8a4487e9b6c86bf7cac641a819e0756600477036
Renan <seu@email.aqui>
2024-11-26 14:56:42 -0300

    jpa consultas avançadas

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -1,12 +1,16 @@
 package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +24,28 @@ public class Produto {
     private String descricao;
     // @Column(name = "preco")
     private BigDecimal preco;
+
+    @ManyToOne
+    private Categoria categoria;
+    private LocalDate dataCadastro = LocalDate.now();
+
+    public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Produto() {
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
 
     public Long getId() {
         return id;
